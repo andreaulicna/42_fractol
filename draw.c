f6b06cc5abb722e2cae7ed58ad016c6961f4a772
aulicna <106449030+andreaulicna@users.noreply.github.com>
2023-10-25 16:40:35 +0200

    Reliably drawing Mandelbrot. Long draw_mandelbrot function split into draw and calculate component. The draw function will eventually be a generic one for all fractals. Also set_cx_cy function added for better readibility.

--- draw.c
+++ draw.c
@@ -6,7 +6,7 @@
 /*   By: aulicna <aulicna@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 08:57:50 by aulicna           #+#    #+#             */
-/*   Updated: 2023/10/25 14:11:29 by aulicna          ###   ########.fr       */
+/*   Updated: 2023/10/25 16:40:21 by aulicna          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,45 +20,55 @@ void	my_mlx_pixel_put(t_data *img, int x, int y, int color)
 	*(unsigned int *)dst = color;
 }
 
-void	draw_mandelbrot(t_data *img, t_fractol *fractol)
+void	set_cx_cy(t_fractol *fractol)
 {
-	int		i;
+	if (fractol->set == 'M')
+	{
+		fractol->cx = fractol->x / fractol->zoom + fractol->offset_x;
+		fractol->cy = fractol->y / fractol->zoom + fractol->offset_y;
+	}
+	else
+	{
+		fractol->cx = fractol->x / fractol->zoom + fractol->offset_x;
+		fractol->cy = fractol->y / fractol->zoom + fractol->offset_y;
+	}
+}
+
+void	calculate_mandelbrot(t_data *img, t_fractol *fractol)
+{
+	int	i;
 	double	x_tmp;
 
+	i = 0;
+	set_cx_cy(fractol);
+	while (fractol->zx * fractol->zx + fractol->zy * fractol->zy < __DBL_MAX__
+		&& i < MAX_ITERATIONS)
+	{
+		x_tmp = fractol->zx * fractol->zx - fractol->zy * fractol->zy
+			+ fractol->cx;
+		fractol->zy = 2. * fractol->zx * fractol->zy + fractol->cy;
+		fractol->zx = x_tmp;
+		i++;
+	}
+	if (i == MAX_ITERATIONS)
+		my_mlx_pixel_put(img, fractol->x, fractol->y, 0x000000);
+	else
+		my_mlx_pixel_put(img, fractol->x, fractol->y, 0xEDEDFF * (i % 255));
+}
+
+// This function will eventually be a generic function draw_fractol 
+// and will be using the calculate functions to draw specific fractals
+void	draw_mandelbrot(t_data *img, t_fractol *fractol)
+{
 	fractol->x = 0;
 	while (fractol->x <= WIN_WIDTH)
 	{
 		fractol->y = 0;
 		while (fractol->y <= WIN_HEIGHT)
 		{
-			i = 0;
 			fractol->zx = 0.;
 			fractol->zy = 0.;
-			fractol->cx = fractol->x / fractol->zoom + fractol->offset_x;
-			fractol->cy = fractol->y / fractol->zoom + fractol->offset_y;
-			while (fractol->zx * fractol->zx + fractol->zy * fractol->zy < __DBL_MAX__
-				&& ++i < MAX_ITERATIONS)
-			{
-				x_tmp = fractol->zx * fractol->zx - fractol->zy * fractol->zy
-					+ fractol->cx;
-				fractol->zy = 2. * fractol->zx * fractol->zy + fractol->cy;
-				fractol->zx = x_tmp;
-			}
-			if (i == MAX_ITERATIONS)
-			{
-			// ft_printf("max\n");
-				my_mlx_pixel_put(img, fractol->x, fractol->y, 0x000000);
-			}
-			else
-			{
-			// ft_printf("not max\n");
-			//	int r = (fractol->x * 255 / WIN_WIDTH) % 256;
-			//	int g = (fractol->y * 255 / WIN_HEIGHT) % 256;
-			//	int b = (fractol->x + fractol->y) % 256;
-			//	int color = (r << 16) | (g << 8) | b;  // RGB color value
-			//	my_mlx_pixel_put(img, fractol->x, fractol->y, color);
-				my_mlx_pixel_put(img, fractol->x, fractol->y, 0xEDEDFF * i);
-			}
+			calculate_mandelbrot(img, fractol);
 			fractol->y++;
 		}
 		fractol->x++;
